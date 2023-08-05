ffec83f0820a5ae3dc7c29dd58ea53fa40e4bf76
Caio Araujo <89320217+ojuaracaio@users.noreply.github.com>
2023-08-05 12:37:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    const selectElement = $("#selectOptions");
+  
+    // Function to fetch data from the server using AJAX
+    function fetchData() {
+      return $.ajax({
+        url: "http://magosdeiliria.ddns.net:5000/ListarDocumentos", // Replace with your actual API endpoint
+        method: "GET",
+        crossDomain: true,
+        dataType: 'jsonp',
+        error: function(xhr, status, error) {
+          console.error("Error fetching data:", error);
+        }
+      });
+    }
+  
+    // Function to update the select element with fetched data
+    function updateSelectOptions(data) {
+      data.forEach((option) => {
+        const optionElement = $("<option>").val(option).text(option);
+        selectElement.append(optionElement);
+      });
+    }
+  
+    // Fetch data and update the select element on page load
+    fetchData().done((data) => updateSelectOptions(data));
+  });
+  
